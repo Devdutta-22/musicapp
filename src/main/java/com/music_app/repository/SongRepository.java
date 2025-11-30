a4f9809e7f8b00e5d57f0847ecf3bc1adf170325
Devdutta Nath <devdutta.24scse1180076@galgotiasuniversity.ac.in>
2025-12-01 04:54:54 +0530

    Enhance SongRepository with search functionality
    Added method to find songs by title or artist name ignoring case.

--- src/main/java/com/music_app/repository/SongRepository.java
+++ src/main/java/com/music_app/repository/SongRepository.java
@@ -2,5 +2,9 @@ package com.music_app.repository;
 
 import com.music_app.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface SongRepository extends JpaRepository<Song, Long> {}
+public interface SongRepository extends JpaRepository<Song, Long> {
+    // This SQL query runs inside the database (Super Fast)
+    List<Song> findByTitleContainingIgnoreCaseOrArtistNameContainingIgnoreCase(String title, String artistName);
+}
