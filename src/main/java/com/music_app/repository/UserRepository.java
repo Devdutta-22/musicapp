3ad49fc9356d89662889906c12318f78538bc851
Devdutta-22 <dev1527b@gmail.com>
2025-12-07 19:47:27 +0530

    Implement findByUsername method in UserRepository
    Added a method to find a user by their username.

--- src/main/java/com/music_app/repository/UserRepository.java
+++ src/main/java/com/music_app/repository/UserRepository.java
@@ -2,5 +2,9 @@ package com.music_app.repository;
 
 import com.music_app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    // This allows us to find a user just by typing their name
+    Optional<User> findByUsername(String username);
+}
