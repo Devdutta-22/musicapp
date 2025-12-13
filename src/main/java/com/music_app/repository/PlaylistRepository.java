8c06227ee4420a88c2c8479a4a6338de27134a55
Devdutta Nath <devdutta.24scse1180076@galgotiasuniversity.ac.in>
2025-12-14 03:06:42 +0530

    Add method to find playlists by user ID

--- src/main/java/com/music_app/repository/PlaylistRepository.java
+++ src/main/java/com/music_app/repository/PlaylistRepository.java
@@ -2,5 +2,9 @@ package com.music_app.repository;
 
 import com.music_app.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List; // <--- Import this
 
-public interface PlaylistRepository extends JpaRepository<Playlist, Long> {}
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    // --- ADD THIS LINE ---
+    List<Playlist> findByUserId(Long userId);
+}
